dfdf5c82aca26cfc2bdf3f79fed0794ca3101066
Itsaso Aguirre Lorenzo <itsaso.aguirre@plainconcepts.com>
2023-01-02 12:05:06 +0100

    Android 12 PendingIntent Fix

--- src/android/src/com/chariotsolutions/nfc/plugin/NfcPlugin.java
+++ src/android/src/com/chariotsolutions/nfc/plugin/NfcPlugin.java
@@ -483,7 +483,9 @@ public class NfcPlugin extends CordovaPlugin implements NfcAdapter.OnNdefPushCom
             Activity activity = getActivity();
             Intent intent = new Intent(activity, activity.getClass());
             intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            pendingIntent = PendingIntent.getActivity(activity, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+            pendingIntent = (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M)? 
+                PendingIntent.getActivity(activity, 0, intent, PendingIntent.FLAG_MUTABLE); : 
+                PendingIntent.getActivity(activity, 0, intent, 0);
         }
     }
 
